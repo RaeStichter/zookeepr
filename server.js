6661c021c6da16b8423d353af346fa2dc331e359
Rae Stichter <raestichter@gmail.com>
2021-01-23 11:33:55 -0500

    added functionality to allow for additional animals to be added to the animals.json.  Validation of user input has also been aded

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
+const fs = require('fs'); //used to write data to the animals.json file
+const path = require('path'); //makes our file system a little more predictable and easier to interface with Heroku
+const { type } = require('os');
 
 // tells heroku where to go
 const PORT = process.env.PORT || 3001;
@@ -7,6 +10,13 @@ const PORT = process.env.PORT || 3001;
 // instantiate the serve
 const app = express();
 
+// ----------------------------------------PARSE INCOMING DATA ----------------------------------------
+// parse incoming string or array data
+app.use(express.urlencoded({ extended:true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// ---------------------------------------- FUNCTIONS ----------------------------------------
 // ability to filter results or what you are looking for
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -53,6 +63,36 @@ function findById(id, animalsArray) {
     return result;
 }
 
+// function to create a new animal
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    //SYNC does not require a call back function, this is not a large file, so this is acceptable here.  Would normally use regual writeFile
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2) //null means we don't want to edit anything.  2 indicates we want to create white space with our values to make it more readable.  W/o this, would be hard to read
+    );
+    return animal
+}
+
+// function to validate the new animal data coming in
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
+// ---------------------------------------- GET DATA FROM THE SERVER ----------------------------------------
 // ROUTE add the route to get the information
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -72,6 +112,22 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+// ---------------------------------------- POST DATA TO THE SERVER ----------------------------------------
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }    
+});
+
+// ---------------------------------------- SERVER TO LISTEN ----------------------------------------
 // get the server to listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
